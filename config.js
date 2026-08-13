1a3e9cb43ef610f5551ad21ac244a34066bec899
Theerawa21 <numpanoof@gmail.com>
2026-08-13 12:47:48 +0700

    Connect production Apps Script API

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   spreadsheetId: '1LVh0yj1yvTv4RtR_L1H6rgAatZNEzpgr3MO-9xIUImw',
-  apiUrl: '',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwa-jdzAJ6C7fW4x3ngWTMLz3FvZkakzYWobrTIBPF4mDeycNKlZli8bmCS1FXNevKA/exec',
   schoolName: 'โรงเรียนเซนต์เทเรซา',
-  dataMode: 'embedded'
+  dataMode: 'api'
 };
